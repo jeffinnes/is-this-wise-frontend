d917a318957697c4921383beb0dc5afba6039bd0
Jeff Innes <contact.me@jeffinnes.dev>
2021-03-22 15:20:02 -0700

    Modify main.js to include global components

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.component('base-button', BaseButton);
+app.component('base-spinner', BaseSpinner);
+
+app.mount('#app');
