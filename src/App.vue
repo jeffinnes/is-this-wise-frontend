c66b4472220e58f089b55bf206305e46cfaebbc0
Jeff Innes <contact.me@jeffinnes.dev>
2021-08-03 16:11:16 -0700

    Clean out unneeded style rules
    These were replaced by the base button component

--- src/App.vue
+++ src/App.vue
@@ -147,56 +147,6 @@ div.user-choice {
   align-items: center;
 }
 
-/* div.user-choice a {
-  border-top: 1px solid #00df7e;
-  background: #24db8c;
-  background: -webkit-gradient(
-    linear,
-    left top,
-    left bottom,
-    from(#00b667),
-    to(#24db8c)
-  );
-  background: -webkit-linear-gradient(top, #00b667, #24db8c);
-  background: -moz-linear-gradient(top, #00b667, #24db8c);
-  background: -ms-linear-gradient(top, #00b667, #24db8c);
-  background: -o-linear-gradient(top, #00b667, #24db8c);
-  padding: 5px 10px;
-  -webkit-border-radius: 8px;
-  -moz-border-radius: 8px;
-  border-radius: 8px;
-  -webkit-box-shadow: rgba(0, 0, 0, 1) 0 1px 0;
-  -moz-box-shadow: rgba(0, 0, 0, 1) 0 1px 0;
-  box-shadow: rgba(0, 0, 0, 1) 0 1px 0;
-  text-shadow: rgba(0, 0, 0, 0.4) 0 1px 0;
-  color: #453645;
-  font-size: 14px;
-  font-family: "Literata", serif;
-  text-decoration: none;
-  vertical-align: middle;
-  font-size: 1.5rem;
-  width: 15rem;
-  height: 5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-div.user-choice a:hover {
-  border-top-color: #00703f;
-  background: #00703f;
-  color: #ccc;
-}
-
-div.user-choice a:active {
-  border-top-color: #014727;
-  background: #014727;
-}
-
-div.user-choice a span {
-  text-align: center;
-} */
-
 /* Quick advice rules */
 
 div.advice-container {
