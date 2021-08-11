4892a7a8d2b6abf58f65fabef6934621ad08d6f8
Jeff Innes <contact.me@jeffinnes.dev>
2021-08-10 23:07:23 -0700

    Login Button component created proved concept with GitHub and Facebook buttons

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,17 +1,45 @@
 <template>
-  <a :href="baseURL + '/auth/github'">Log In with GitHub</a>
+  <div class="login-container">
+    <base-card class="login-card">
+      <login-button
+        :href="baseURL + '/auth/github'"
+        provider="github"
+        :iconSrc="githubLogo"
+        iconAlt="GitHub Octocat Logo"
+        text="Log in with GitHub"
+      ></login-button>
+      <login-button
+        :href="baseURL + '/auth/facebook'"
+        provider="facebook"
+        :iconSrc="facebookLogo"
+        iconAlt="Facebook Logo"
+        text="Log in with Facebook"
+      ></login-button>
+    </base-card>
+  </div>
 </template>
 
 <script>
+import LoginButton from '../components/ui/LoginButton.vue';
+import githubLogo from '../assets/oauth-logos/github/GitHub-Mark-120px-plus.png';
+import facebookLogo from '../assets/oauth-logos/facebook/f_logo_RGB-Blue_58.png';
+
 export default {
+  components: {
+    LoginButton,
+  },
   data() {
     return {
       baseURL: process.env.VUE_APP_BACKEND_BASE,
+      githubLogo,
+      facebookLogo,
     };
   },
 };
 </script>
 
-<style>
-
+<style scoped>
+  div.login-container {
+    grid-column: 5 / 9;
+  }
 </style>
